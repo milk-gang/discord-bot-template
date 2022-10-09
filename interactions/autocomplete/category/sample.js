d60fd2acfae895bf78ab72b3f0031f6491de7683
Milk <officialjmasmila@gmail.com>
2022-10-09 09:02:37 +0800

    Initial commit

---
+++ interactions/autocomplete/category/sample.js
@@ -0,0 +1,14 @@
+module.exports = {
+	name: 'sample',
+	async execute(interaction) {
+        if (interaction.isAutocomplete()) {
+            const focusedValue = interaction.options.getFocused();
+            const choices = ['your', 'choices'];
+            const filtered = choices.filter((choice) => choice.startsWith(focusedValue));
+
+            await interaction.respond(
+                filtered.map((choice) => ({ name: choice, value: choice }))
+            );
+        }
+	}
+};
